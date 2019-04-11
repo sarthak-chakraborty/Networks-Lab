87474b85ea6866f97c62b4c38c6835c12d13f3ae
Sarthak Chakraborty <sarthak.chakraborty@gmail.com>
2019-04-11 21:49:58 +0530

    Checksum not included

--- Assignment 8/mytraceroute_16CS30044.c
+++ Assignment 8/mytraceroute_16CS30044.c
@@ -19,50 +19,42 @@
 #include <time.h>
 
 
-#define BUFFER 100
+#define BUFFER 52
 #define MAX 100
+#define MAX_TTL 20
 
 
 /////////////////////////////////////////////////////////////////////////////
-uint16_t udp_checksum(struct udphdr *p_udp_header, size_t len, uint32_t src_addr, uint32_t dest_addr)
+unsigned int checksum(uint16_t *usBuff, int isize)
 {
-	const uint16_t *buf = (const uint16_t*)p_udp_header;
-	uint16_t *ip_src = (void*)&src_addr, *ip_dst = (void*)&dest_addr;
-	uint32_t sum;
-	size_t length = len;
+	unsigned int cksum=0;
+	for(;isize>1;isize-=2){
+	cksum+=*usBuff++;
+       }
+	if(isize==1){
+	 cksum+=*(uint16_t *)usBuff;
+        }
 
-	// Calculate the sum
-	sum = 0;
-	while (len > 1){
-		sum += *buf++;
-		if (sum & 0x80000000)
-		  sum = (sum & 0xFFFF) + (sum >> 16);
-		len -= 2;
-	}
-
-	if (len & 1)
-		// Add the padding if the packet lenght is odd
-		sum += *((uint8_t*)buf);
-
-	// Add the pseudo-header
-	sum += *(ip_src++);
-	sum += *ip_src;
-
-	sum += *(ip_dst++);
-	sum += *ip_dst;
-
-	sum += htons(IPPROTO_UDP);
-	sum += htons(length);
-
-	// Add the carries
-	while (sum >> 16)
-		sum = (sum & 0xFFFF) + (sum >> 16);
-
-	// Return the one's complement of sum
-	return (uint16_t)~sum;
+	return (cksum);				//calculate the checksum and return 
 }
 
+uint16_t check_udp_sum(uint8_t *buffer, int len)			//Standard UDP Checksum Code
+{
+    unsigned long sum=0;
+	struct iphdr *tempI=(struct iphdr *)(buffer);
+	struct udphdr *tempH=(struct udphdr *)(buffer+sizeof(struct iphdr));
+	tempH->check=0;
+	sum=checksum((uint16_t *)&(tempI->saddr) ,8);	//Callinf the checksum function
+	sum+=checksum((uint16_t *) tempH,len);
 
+	sum+=ntohs(IPPROTO_UDP+len);
+
+	sum=(sum>>16)+(sum & 0x0000ffff);
+	sum=((sum&0)>>16);
+
+	return (uint16_t)(sum);			//Return the checksum
+	
+}
 
 
 
@@ -86,6 +78,8 @@ int main(int argc, char *argv[]){
 	}
 
 	adr = (struct in_addr **)ip->h_addr_list;
+	printf("\nDestination Domain Requested: ");
+	printf("%s (%s), %d hops max\n\n",domain, inet_ntoa(*adr[0]), MAX_TTL);
 
 
 	int icmp_fd, udp_fd, count, ttl = 1;
@@ -95,7 +89,7 @@ int main(int argc, char *argv[]){
 	struct udphdr *udp_header;
 	struct iphdr *recv_ip_header;
 	struct icmphdr *icmp_header;
-	char buff[52 + sizeof(struct udphdr) + sizeof(struct iphdr)];
+	char buff[BUFFER + sizeof(struct udphdr) + sizeof(struct iphdr)];
 	char mssg[MAX];
 	fd_set rdfs;
 	struct timeval tv;
@@ -107,7 +101,7 @@ int main(int argc, char *argv[]){
     udp_header = (struct udphdr *)(buff + sizeof(struct iphdr));
     recv_ip_header = (struct iphdr *)mssg;
     icmp_header = (struct icmphdr *)(mssg + sizeof(struct iphdr));
-    memset(buff, 0, BUFFER);
+    memset(buff, 0, BUFFER + sizeof(struct udphdr) + sizeof(struct iphdr));
 
 
     // Create raw sockets
@@ -141,23 +135,24 @@ int main(int argc, char *argv[]){
 		printf("[Error] Setsockopt failed\n");
 		exit(0);
 	}
-	if(setsockopt(icmp_fd, IPPROTO_IP, IP_HDRINCL, &(int){1}, sizeof(int)) < 0){
-		printf("[Error] Setsockopt failed\n");
-		exit(0);
-	}
 	// Bind to the raw sockets 
-	if(bind(udp_fd, &udp_addr, sizeof(udp_addr)) < 0){
+	if(bind(udp_fd, (struct sockaddr *)&udp_addr, sizeof(udp_addr)) < 0){
 		printf("[Error] Binding failed\n");
 		exit(0);
 	}
-	if(bind(icmp_fd, &icmp_addr, sizeof(icmp_addr)) < 0){
+	if(bind(icmp_fd, (struct sockaddr *)&icmp_addr, sizeof(icmp_addr)) < 0){
 		printf("[Error] Binding failed\n");
 		exit(0);
 	}
 
 
-	printf("Hop_Count(TTL Value)\tIP Address\t    Response_time\n");
-	printf("--------------------\t----------\t    -------------\n");
+	// Fill in random bits in payload of UDP
+	int i, r;
+	for(i=0; i<51; i++){
+		r = rand()%26 + 65;
+		buff[i + sizeof(struct iphdr) + sizeof(struct udphdr)] = (char)r;
+	}
+	buff[i + sizeof(struct iphdr) + sizeof(struct udphdr)] = '\0';
 
 
 	// Set the IP headers
@@ -168,7 +163,7 @@ int main(int argc, char *argv[]){
     ip_header->tot_len = htons(sizeof(buff));
     ip_header->id = htonl(id);
     ip_header->frag_off = 0;
-    ip_header->protocol = IPPROTO_IP;
+    ip_header->protocol = IPPROTO_UDP;
     ip_header->saddr = udp_addr.sin_addr.s_addr;
     ip_header->daddr = inet_addr(inet_ntoa(*adr[0]));
 
@@ -176,19 +171,11 @@ int main(int argc, char *argv[]){
     udp_header->source = udp_addr.sin_port;
     udp_header->dest = htons(32164);
 	udp_header->len = htons(sizeof(buff) - sizeof(struct iphdr));
-	udp_header->check = udp_checksum(udp_header, sizeof(buff) - sizeof(struct iphdr), ip_header->saddr, ip_header->daddr);
+	udp_header->check = check_udp_sum(buff, sizeof(buff) - sizeof(struct iphdr));
 
 
-	while(1){
+	while(ttl<=MAX_TTL){
 		ip_header->ttl = ttl;	// Set the ttl field of IP header
-
-		// Fill in random bits in payload of UDP
-		int i, r;
-		for(i=0; i<51; i++){
-			r = rand()%26 + 65;
-			buff[i + sizeof(struct iphdr) + sizeof(struct udphdr)] = (char)r;
-		}
-		buff[i + sizeof(struct iphdr) + sizeof(struct udphdr)] = '\0';
 
 		// Send the IP packet to the server
 		sendto(udp_fd, buff, ntohs(ip_header->tot_len), 0,  (struct sockaddr *)&dest_addr, sizeof(dest_addr));
@@ -217,26 +204,35 @@ int main(int argc, char *argv[]){
 				
 				// If ICMP message is of 'TIME LIMIT EXCEEDED', increase ttl and loop again
 				if(icmp_header->type == 11){
-					printf("\t%d\t\t%s\t     %fms\n",ttl, (char *)inet_ntoa(*(struct in_addr*)&recv_ip_header->saddr), (t2.tv_usec-t1.tv_usec)/1000.0);
-					ttl++;
-					id++;
-					break;
+					struct iphdr *ip_recv_header = (struct iphdr *)(mssg + sizeof(struct iphdr) + sizeof(struct icmphdr));
+
+					if(inet_ntoa(*(struct in_addr*)&ip_recv_header->daddr) == inet_ntoa(*adr[0])){
+						printf("Hop_Count(%d)\t%s\t%fms\n",ttl, (char *)inet_ntoa(*(struct in_addr*)&recv_ip_header->saddr), (t2.tv_usec-t1.tv_usec)/1000.0);
+						ttl++;
+						id++;
+						break;
+					}
 				}
 				// If ICMP message is 'DESTINATION UNREACHABLE', break from loop
 				else if(icmp_header->type == 3){
-					printf("\t%d\t\t%s\t     %fms\n",ttl, (char *)inet_ntoa(*(struct in_addr*)&recv_ip_header->saddr), (t2.tv_usec-t1.tv_usec)/1000.0);
-					break;
+					if(inet_ntoa(*(struct in_addr*)&recv_ip_header->saddr) == inet_ntoa(*adr[0])){
+						printf("Hop_count(%d)\t%s\t%fms\n",ttl, (char *)inet_ntoa(*(struct in_addr*)&recv_ip_header->saddr), (t2.tv_usec-t1.tv_usec)/1000.0);
+						break;
+					}	
 				}
 	        }
 	        count++;
 		}
 		if(count == 3){
-			printf("\t%d\t\t    *\t\t     *\n",ttl);
+			printf("Hop_Count(%d)\t*\t*\n",ttl);
 			ttl++;
 			id++;
 		}
 		else if(icmp_header->type == 11) continue;
 		else break;		
 	}
+	close(udp_fd);
+	close(icmp_fd);
+
 	return 0;
 }
